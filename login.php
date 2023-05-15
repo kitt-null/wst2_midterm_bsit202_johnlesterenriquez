f5bebe4094043eca07ee4c17f64cb0d39d2dbb6e
kitt-null <jl.enriquez.0530@gmail.com>
2023-05-14 19:03:03 -0700

    update_login.php_form

--- login.php
+++ login.php
@@ -6,13 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
 
 </head>
 
 <body>
-    
+    <main>
+        <div>
+            <h1>Log In</h1>
+            <div>
+                <form>
+                    <div>
+                        <label for="login_email" name="login_email" id="login_email">Email</label>
+                        <input type="text">
+                    </div>
 
+                    <div>
+                    <label for="login_password" name="login_password" id="login_password">Password</label>
+                        <input type="password">
+                    </div>
+
+                    <div>
+                        <button>Log In</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+
+    <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/js.js"></script>
     
 </body>
