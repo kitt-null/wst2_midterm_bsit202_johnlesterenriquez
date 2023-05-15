48bfcccde0ff366733057d8d1675bf797e36c17d
kitt-null <jl.enriquez.0530@gmail.com>
2023-05-14 19:03:32 -0700

    update_registration.php_form

---
+++ registration.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+    <main>
+        <div>
+            <h1>Registration</h1>
+            <div>
+                <form>
+                    <div>
+                        <label for="reg_email" name="reg_email" id="reg_email">Email</label>
+                        <input type="text">
+                    </div>
+
+                    <div>
+                    <label for="reg_password" name="reg_password" id="reg_password">Password</label>
+                        <input type="password">
+                    </div>
+
+                    <div>
+                    <label for="reg_name" name="reg_name" id="reg_name">Name</label>
+                        <input type="text">
+                    </div>
+
+                    <div>
+                        <button>Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/js.js"></script>
+    
+</body>
+
+</html>
